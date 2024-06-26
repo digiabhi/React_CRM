943f08bb4d1c94212125d643674690944528ab1b
Abhishek Singh <singhabhishek111213@gmail.com>
2024-06-26 15:05:01 +0530

    Added react router setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import "./index.css";
 
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
 import store from "./Redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
