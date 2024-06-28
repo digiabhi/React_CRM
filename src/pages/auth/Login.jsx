6dd10dfb2eeedd4c08ef866c8f48e8ef8cdb773f
Abhishek Singh <singhabhishek111213@gmail.com>
2024-06-28 11:02:43 +0530

    Added async thunk for login

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,6 +1,28 @@
-import React from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+
+import { login } from "../../Redux/Slices/AuthSlice";
 
 function Login() {
+
+  const dispatch = useDispatch();
+
+  const [loginDetails, setLoginDetails] = useState({
+    email: "",
+    password: ""
+  });
+
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+    console.log(name, value);
+    setLoginDetails({ ...loginDetails, [name]: value });
+  }
+  function onSubmit() {
+    if (!loginDetails.email || !loginDetails.password) return;
+    console.log(loginDetails);
+    const response = dispatch(login(loginDetails));
+    console.log(response);
+  }
   return (
     <div className="flex justify-center items-center h-[90vh]">
       <div className="card bg-primary text-primary-content w-96">
@@ -10,14 +32,18 @@ function Login() {
           </div>
           <div className="w-full">
             <input
+              onChange={handleInputChange}
+              name="email"
               type="text"
-              placeholder="Enter User Id"
+              placeholder="Enter Email"
               className="input input-bordered input-primary w-full max-w-xs text-white"
               autoComplete="one-time-code"
             />
           </div>
           <div className="w-full">
             <input
+              onChange={handleInputChange}
+              name="password"
               type="password"
               placeholder="Enter Password"
               className="input input-bordered input-primary w-full max-w-xs text-white"
@@ -25,7 +51,7 @@ function Login() {
             />
           </div>
           <div className="card-actions w-full mt-4">
-            <button className="btn btn-warning w-full font-bold text-xl">
+            <button className="btn btn-warning w-full font-bold text-xl" onClick={onSubmit}>
               Submit
             </button>
           </div>
