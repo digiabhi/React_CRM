9cc370e80b89c5746c3dd31a518e41418aef0755
Abhishek Singh <singhabhishek111213@gmail.com>
2024-07-25 16:53:22 +0530

    Added tickets slice for maintaining ticket state

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import authSliceReducer from "../Redux/Slices/AuthSlice";
+import ticketSliceReducer from "../Redux/Slices/TicketSlice";
 const store = configureStore({
   reducer: {
     auth: authSliceReducer,
+    tickets: ticketSliceReducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
   devTools: true,
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,26 @@
+import { useEffect } from "react";
 import { BsFillPencilFill } from "react-icons/bs";
+import { useDispatch, useSelector } from "react-redux";
 
 import Card from "../../components/Card";
 import HomeLayout from "../../layouts/HomeLayout";
+import { getAllTicketsForTheUser } from "../../Redux/Slices/TicketSlice";
 
 
 function Home() {
+    const authState = useSelector((state) => state.auth);
+    const ticketState = useSelector((state) => state.tickets);
+
+    const dispatch = useDispatch();
+
+    async function loadTickets() {
+        const response = await dispatch(getAllTicketsForTheUser());
+        console.log(response);
+    }
+
+    useEffect(() => {
+        loadTickets();
+    }, [authState.token]);
     return (
         <HomeLayout>
             {/* <Card >
