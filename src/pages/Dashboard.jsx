fdc00fbc4581817b1361400a2bc596dc2c43d6c2
Abhishek Singh <singhabhishek111213@gmail.com>
2024-08-08 12:51:09 +0530

    Added react data component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,3 +1,4 @@
+import DataTable from 'react-data-table-component';
 import { AiOutlineDownload } from "react-icons/ai";
 import { usePDF } from "react-to-pdf";
 
@@ -7,6 +8,67 @@ import HomeLayout from "../layouts/HomeLayout";
 function Dashboard() {
     const [ticketState] = useTickets();
     const { toPDF, targetRef } = usePDF({ filename: 'page.pdf' });
+    const customStyles = {
+        rows: {
+            style: {
+                minHeight: '72px', // override the row height
+                fontSize: '18px'
+            },
+        },
+        headCells: {
+            style: {
+                paddingLeft: '8px', // override the cell padding for head cells
+                paddingRight: '8px',
+            },
+        },
+        cells: {
+            style: {
+                paddingLeft: '8px', // override the cell padding for data cells
+                paddingRight: '8px',
+            },
+        },
+    };
+
+    const columns = [
+        {
+            name: 'Ticket Id',
+            selector: row => row._id,
+            reorder: true,
+        },
+        {
+            name: 'Title',
+            selector: row => row.title,
+            reorder: true,
+        },
+        {
+            name: 'Description',
+            selector: row => row.description,
+            reorder: true,
+        },
+        {
+            name: 'Reporter',
+            selector: row => row.assignedTo,
+            reorder: true,
+        },
+        {
+            name: 'Priority',
+            selector: row => row.ticketPriority,
+            reorder: true,
+            sortable: true
+        },
+        {
+            name: 'Assignee',
+            selector: row => row.assignee,
+            reorder: true,
+        },
+        {
+            name: 'Status',
+            selector: row => row.status,
+            reorder: true,
+            sortable: true
+        },
+    ];
+    const ExpandedComponent = ({ data }) => <pre>{JSON.stringify(data, null, 2)}</pre>;
     return (
         <HomeLayout>
             <div className="min-h-[90vh] flex flex-col items-center justify-center gap-2">
@@ -14,8 +76,7 @@ function Dashboard() {
                     Tickets Records <AiOutlineDownload onClick={() => toPDF()} className="cursor-pointer inline" />
                 </div>
 
-                <div className="flex flex-col w-full" ref={targetRef}>
-                    {/* Title Row */}
+                {/* <div className="flex flex-col w-full" ref={targetRef}>
                     <div className="flex justify-between items-center gap-3 bg-purple-600 text-white font-bold px-2 py-2">
                         <div className="table-title basis-[8%] justify-start">
                             Ticket Id
@@ -39,8 +100,6 @@ function Dashboard() {
                             Status
                         </div>
                     </div>
-
-                    {/* Ticket Details */}
                     {ticketState && ticketState.ticketList.map(ticket => {
                         return (
                             <div key={ticket._id} className="my-4 text-sm flex justify-between items-center gap-3 bg-gray-100 hover:bg-gray-400 transition-all ease-in-out duration-300 text-black px-2 py-2">
@@ -68,6 +127,10 @@ function Dashboard() {
                             </div>
                         );
                     })}
+                </div> */}
+
+                <div ref={targetRef}>
+                    {ticketState && <DataTable columns={columns} data={ticketState.ticketList} expandableRows expandableRowsComponent={ExpandedComponent} customStyles={customStyles} />}
                 </div>
 
             </div>
