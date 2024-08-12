04abeb1e2f8d052fc18bf1a3ad095d830bcb81c5
Abhishek Singh <singhabhishek111213@gmail.com>
2024-08-12 12:52:51 +0530

    Added generic user update handler

--- src/components/UserDetailsModal.jsx
+++ src/components/UserDetailsModal.jsx
@@ -5,16 +5,18 @@ import axiosInstance from "../config/axiosInstance";
 
 function UserDetailsModal({ user, resetTable }) {
     const [userDisplay, setUserDisplay] = useState(user);
-    async function handleStatusChange(e) {
+    async function handleUserChange(e) {
         try {
-            const dropdown = document.getElementById("userstatus-dropdown");
+            const ul = e.target.parentNode.parentNode;
+            const name = ul.getAttribute("name");
+            const dropdown = document.getElementById(`${name}Dropdown`);
             dropdown.open = !dropdown.open;
             toast("Updating the user...");
             const response = await axiosInstance.patch("user/updateUser", {
                 userId: userDisplay.id,
                 updates: {
                     ...userDisplay,
-                    userStatus: e.target.textContent
+                    [name]: e.target.textContent
                 }
             }, {
                 headers: {
@@ -26,11 +28,12 @@ function UserDetailsModal({ user, resetTable }) {
                 toast.success("Successfully updated the user");
                 const user = response?.data?.result;
                 setUserDisplay({
+                    ...userDisplay,
                     name: user.name,
                     email: user.email,
                     userStatus: user.userStatus,
                     userType: user.userType,
-                    clientName: user.clientName
+                    clientName: user.clientName,
                 });
                 resetTable();
             }
@@ -46,9 +49,9 @@ function UserDetailsModal({ user, resetTable }) {
                 <p className="py-4">Client Name: <span className="text-yellow-500"> {userDisplay.clientName}</span></p>
                 <p className="py-4">Status:
                     <span className="text-yellow-500">
-                        <details className="dropdown ml-2" id="userstatus-dropdown">
+                        <details className="dropdown ml-2" id="userStatusDropdown">
                             <summary className="btn m-1">{userDisplay.userStatus}</summary>
-                            <ul onClick={handleStatusChange} className="menu dropdown-content bg-base-100 text-white rounded-box z-[1] w-52 p-2 shadow">
+                            <ul name="userStatus" onClick={handleUserChange} className="menu dropdown-content bg-base-100 text-white rounded-box z-[1] w-52 p-2 shadow">
                                 <li><a>approved</a></li>
                                 <li><a>suspended</a></li>
                                 <li><a>rejected</a></li>
@@ -56,7 +59,18 @@ function UserDetailsModal({ user, resetTable }) {
                         </details>
                     </span>
                 </p>
-                <p className="py-4">Type: <span className="text-yellow-500"> {userDisplay.userType}</span></p>
+                <p className="py-4">Type:
+                    <span className="text-yellow-500">
+                        <details className="dropdown ml-2" id="userTypeDropdown">
+                            <summary className="btn m-1">{userDisplay.userType}</summary>
+                            <ul name="userType" onClick={handleUserChange} className="menu dropdown-content bg-base-100 text-white rounded-box z-[1] w-52 p-2 shadow">
+                                <li><a>customer</a></li>
+                                <li><a>engineer</a></li>
+                                <li><a>admin</a></li>
+                            </ul>
+                        </details>
+                    </span>
+                </p>
                 <p className="py-4">Email: <span className="text-yellow-500"> {userDisplay.email}</span></p>
             </div>
             <form method="dialog" className="modal-backdrop">
