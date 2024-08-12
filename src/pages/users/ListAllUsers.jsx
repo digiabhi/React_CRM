ae558768e0e3f7ff6d32306d85f8ecfe3862a9a6
Abhishek Singh <singhabhishek111213@gmail.com>
2024-08-12 11:26:09 +0530

    Added user details popup modal

--- src/pages/users/ListAllUsers.jsx
+++ src/pages/users/ListAllUsers.jsx
@@ -7,6 +7,13 @@ import HomeLayout from "../../layouts/HomeLayout";
 function ListAllUsers() {
 
     const [userList, setUserList] = useState([]);
+    const [userDisplay, setUserDisplay] = useState({
+        name: '',
+        email: '',
+        userType: '',
+        userStatus: '',
+        clientName: ''
+    });
     const columns = [
         {
             name: 'User Id',
@@ -53,11 +60,26 @@ function ListAllUsers() {
 
     return (
         <HomeLayout >
-            <div className="min-h-[90vh] flex items-center justify-center">
+            <div className="min-h-[90vh] flex flex-col items-center justify-center">
+                <h1 className="text-center font-bold text-5xl mb-4 text-yellow-500">Users List</h1>
                 {userList && <DataTable
+                    onRowClicked={(row) => { setUserDisplay({ name: row.name, clientName: row.clientName, email: row.email, userStatus: row.userStatus, userType: row.userType }); document.getElementById('my_modal_2').showModal(); }}
                     columns={columns}
                     data={userList}
                 />}
+                <dialog id="my_modal_2" className="modal">
+                    <div className="modal-box">
+                        <h3 className="font-bold text-lg">User Details</h3>
+                        <p className="py-4">Name: {userDisplay.name}</p>
+                        <p className="py-4">Client Name: {userDisplay.clientName}</p>
+                        <p className="py-4">Status: {userDisplay.userStatus}</p>
+                        <p className="py-4">Type: {userDisplay.userType}</p>
+                        <p className="py-4">Email: {userDisplay.email}</p>
+                    </div>
+                    <form method="dialog" className="modal-backdrop">
+                        <button>close</button>
+                    </form>
+                </dialog>
             </div>
         </HomeLayout >
     );
