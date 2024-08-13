fa22f7c7c43115b8e0d04da53655bd3dea6be0ab
Abhishek Singh <singhabhishek111213@gmail.com>
2024-08-13 11:42:49 +0530

    Added async thunk for creating new ticket

--- src/Redux/Slices/TicketSlice.js
+++ src/Redux/Slices/TicketSlice.js
@@ -54,6 +54,26 @@ export const updateTicket = createAsyncThunk('tickets/updateTicket', async (tick
     }
 });
 
+export const createTicket = createAsyncThunk('tickets/createTicket', async (ticket) => {
+    try {
+        const response = axiosInstance.post(`ticket`,
+            ticket, // req body
+            {
+                headers: {
+                    'x-access-token': localStorage.getItem('token')
+                }
+            });
+        toast.promise(response, {
+            success: "Successfully created the ticket",
+            loading: "Creating the ticket",
+            error: "Something went wrong"
+        });
+        return await response;
+    } catch (error) {
+        console.log(error);
+    }
+});
+
 const ticketSlice = createSlice({
     name: "tickets",
     initialState,
@@ -96,6 +116,21 @@ const ticketSlice = createSlice({
                     if (ticket._id == updatedTicket._id) return updatedTicket;
                     return ticket;
                 });
+                state.ticketDistribution = {
+                    open: 0,
+                    inProgress: 0,
+                    resolved: 0,
+                    onHold: 0,
+                    cancelled: 0
+                };
+                state.downloadedTickets.forEach(ticket => {
+                    state.ticketDistribution[ticket.status] = state.ticketDistribution[ticket.status] + 1;
+                });
+            }).addCase(createTicket.fulfilled, (state, action) => {
+                if (action?.payload?.data == undefined) return;
+                const newTicket = action.payload.data;
+                state.downloadedTickets.push(newTicket);
+                state.ticketList = state.downloadedTickets;
                 state.ticketDistribution = {
                     open: 0,
                     inProgress: 0,
--- src/pages/tickets/CreateTicket.jsx
+++ src/pages/tickets/CreateTicket.jsx
@@ -1,21 +1,73 @@
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useDispatch, useSelector } from "react-redux";
+
+import useTickets from "../../hooks/useTickets";
 import HomeLayout from "../../layouts/HomeLayout";
+import { createTicket } from "../../Redux/Slices/TicketSlice";
 
 function CreateTicket() {
+    const auth = useSelector((state) => state.auth);
+    const dispatch = useDispatch();
+    useTickets();
+    const [ticket, setTicket] = useState({
+        title: '',
+        description: '',
+        ticketPriority: 3,
+        status: "open",
+        clientName: auth.data.clientName
+    });
+
+    function handleFormChange(e) {
+        const { name, value } = e.target;
+        setTicket({
+            ...ticket,
+            [name]: value
+        });
+    }
+
+    async function onFormSubmit(e) {
+        e.preventDefault();
+        if (!ticket.title || !ticket.description) {
+            toast.error("Title and description are mandatory");
+            return;
+        }
+        const response = dispatch(createTicket(ticket));
+        if (response?.payload?.status == 201) {
+            setTicket({
+                title: '',
+                description: '',
+                ticketPriority: 3,
+                status: "open",
+                clientName: auth.data.clientName
+            });
+        }
+    }
+
     return (
         <HomeLayout>
             <div className="min-h-[90vh] flex items-center justify-center">
-                <form className="min-w-[40rem] border p-20 border-sky-500 rounded-lg hover:bg-sky-900 transition-all ease-in-out duration-300">
+                <form className="min-w-[40rem] border p-20 border-sky-500 rounded-lg hover:bg-sky-900 transition-all ease-in-out duration-300" onSubmit={onFormSubmit}>
                     <h1 className="text-3xl font-semibold text-white text-center">Create new ticket</h1>
                     <div className="form-control w-full my-4">
                         <label className="label flex-col">
                             <span className="label-text text-white text-lg">What is title of the issue?</span>
-                            <input type="text" placeholder="Type here" className="input input-bordered w-full bg-white input-primary text-black" />
+                            <input
+                                type="text"
+                                placeholder="Type here"
+                                className="input input-bordered w-full bg-white input-primary text-black"
+                                name="title"
+                                value={ticket.title}
+                                onChange={handleFormChange} />
                         </label>
                     </div>
                     <div className="form-control w-full my-4">
                         <label className="label flex-col">
                             <span className="label-text text-white text-lg">Please describe your issue</span>
                             <textarea
+                                value={ticket.description}
+                                onChange={handleFormChange}
+                                name="description"
                                 className="p-2 w-full bg-white text-black resize-none rounded-md"
                                 placeholder="Type your issue here"
                                 rows="7">
